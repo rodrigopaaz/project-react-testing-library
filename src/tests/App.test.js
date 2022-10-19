4d802a8b904e085f38d3e12b6d351d212ee8a770
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-10-19 16:52:45 -0300

    01 - Corrigindo Lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
-import { getByText, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
