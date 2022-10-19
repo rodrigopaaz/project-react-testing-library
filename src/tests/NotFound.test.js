ded0c0824ef2a7745372321ceb05f2a3f3b3cbd1
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-10-19 18:51:16 -0300

    04 concluido o requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verifica o funcionamento do componente NotFound', () => {
+  it('Verifica se o Heading está presente no componente', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/NotFound');
+    });
+    const MSG = 'Page requested not found';
+    const IMAGE = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const checkImage = screen.getByRole('img');
+    const checkMSG = screen.getByRole('heading', { level: 2 });
+    expect(checkImage.src).toBe(IMAGE);
+    expect(checkMSG).toHaveTextContent(MSG);
+  });
+});
